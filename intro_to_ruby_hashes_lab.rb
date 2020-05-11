0712ba701220687d33b4795d6f9ae8d771df462b
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-11 06:04:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,6 @@
 def base_hash
   base_hash = {
     :railroads => {
-      pieces: "4",
     }
   }
 
@@ -9,8 +8,12 @@ end
 
 def monopoly_with_second_tier
   base_hash = {
-    :railroads => {}
-    
+    :railroads => {
+      pieces: "4", 
+      rent_in_dollars: {},
+      names: {},
+    }
+  }    
 end
 
 def monopoly_with_third_tier
